7950eea8e0eb7ab5e4861d88509579ab52a9eba7
Xun Zhou <segentor@gmail.com>
2020-04-27 00:31:35 +0200

    fix outline problem

--- src/components/NavBarDemo.tsx
+++ src/components/NavBarDemo.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./app.scss";
 import { Tab, NavBarItem, Article, Button, Page, Msg } from "react-weui";
 
 export default class NavBarDemo extends React.Component {
@@ -6,18 +7,14 @@ export default class NavBarDemo extends React.Component {
     return (
       <Tab type="navbar">
         <NavBarItem label="Nav1">
-          <Article>
-            <section>
-              <Page className="msg_success">
-                <Msg
-                  type="success"
-                  title="Action Success"
-                  description="We have received your feedback"
-                  buttons={[]}
-                />
-              </Page>
-              <Button>OK</Button>
-            </section>
+          <Article className="article--full">
+            <Msg
+              type="success"
+              title="Action Success"
+              description="We have received your feedback"
+              buttons={[]}
+            />
+            <Button>OK</Button>
           </Article>
         </NavBarItem>
         <NavBarItem label="Nav2">
@@ -27,6 +24,18 @@ export default class NavBarDemo extends React.Component {
               <h2 className="title">Heading</h2>
               <section>
                 <h3>3.1 Title</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                </p>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
